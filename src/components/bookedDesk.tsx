7a72c085f50acfb67ed3e622af0d4e1f524cc113
Louis Nokes <79872690+LouisNokes@users.noreply.github.com>
2022-10-11 15:47:02 +0100

    Updated booked desk

--- src/components/bookedDesk.tsx
+++ src/components/bookedDesk.tsx
@@ -8,7 +8,7 @@ interface BookedProps {
 export const BookedDesk: FC<BookedProps> = () => {
     return (
         <div className='Second-comp'>
-
+            <h1>Booked desk</h1>
         </div>
     );
 };
