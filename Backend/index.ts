8987c1d0e43d14f9ff99611f9d7f95d0a08f906e
Ewrdad <ewrdad@protonmail.com>
2022-10-11 16:03:01 +0100

    Added templates + simple user auth
    Added comments to backend index.ts, also implemented user auth as example

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,1 +1,58 @@
-console.log("Working on desk system");
+//Backend for server. Running API
+import express from "express";
+const app = express();
+
+//app.get('api/', (req,res) => { res.status(200).send("succcess")});
+//app.get('api/:id', (req,res) => { res.status(200).send(req.query.id)});
+//app.post('api/', (req,res) => {req.send(req.params)});
+
+
+
+//Check Username against list
+
+//Import custom Module
+const authe = require("./backElements/auth.ts");
+//Function to call
+function authentication(userID : String ){
+
+if(authe.checkUser(userID)){
+    return authe.checkUser(userID);
+}
+else{
+    return null
+}
+};
+//When requset to '/api/user/' received run authentication
+app.get('/api/user/:id', (req,res) => {
+    const user = authentication(req.params.id);
+
+    if(user){
+        res.send(user);
+    }
+    else{
+        res.status(404).send("User Not found");
+    }
+    
+});
+
+//Get Booking for a given seat
+
+//Is Seat Booked
+
+//Check for booked seat for a given period
+
+//Book Seat for given period
+
+//Get Seats Given Site
+
+//Get seat details
+
+
+
+
+
+
+
+
+//Listens for request, Should be last to run, Keeps application running 
+app.listen(8000, () => console.log('Listening for queries on port 8000')); 
