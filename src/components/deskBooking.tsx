66faf9104b8d454b85ec464573ceb72f3379a2cf
DavidR794 <105678612+DavidR794@users.noreply.github.com>
2022-10-11 18:08:51 +0100

    Updated
    Updated the placeholders.
    Just need to put them in positions.
    We also need to rationalise CSS files
    and not use camel case on names of files as this could lead to errors

--- src/components/deskBooking.tsx
+++ src/components/deskBooking.tsx
@@ -1,17 +1,36 @@
-import React, { FC } from 'react';
-import '../App.css';
+import React, { FC } from "react";
+import "../App.css";
 
-interface bookingProps {
+interface bookingProps {}
 
-}
-
+//
 export const BookingDesk: FC<bookingProps> = () => {
-    return (
-        <div className='Main-comp'>
-            <input placeholder='Title'></input>
-            <input placeholder='Email'></input>
-            <button>Submit</button>
+  return (
+    <div className="Main-comp" onSubmit={() => console.log("submit")}>
+      <form className="form-element">
+        <div style={{ display: "flex", color: "red", flexDirection: "column" }}>
+          <input placeholder="Title" />
+          <input placeholder="Email" />
         </div>
-    );
-};
 
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <input placeholder="Location" />
+            <input placeholder="Desk" />
+          </div>
+
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <input placeholder="Date" />
+          </div>
+
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <input placeholder="Time From" />
+            <input placeholder="Time Til" />
+          </div>
+        </div>
+        <input placeholder="Description" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
