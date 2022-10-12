d5c0b49b87ad4e95a3b4b86a22c38314bf70859e
Ewrdad <ewrdad@protonmail.com>
2022-10-12 16:10:47 +0100

    Created Base functions
    Created functions and handle submit button for deskBooking form

--- src/components/deskBooking.tsx
+++ src/components/deskBooking.tsx
@@ -8,7 +8,7 @@ interface bookingProps {
 
 }
 
-const location = [
+const locations = [
     'Manchester', 'Gloucester'
 ];
 
@@ -19,18 +19,35 @@ const seatNum = [
 const time = ['11:00'];
 
 export const BookingDesk: FC<bookingProps> = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [dates, setDates] = useState<any>();
+    const [loc, setLoc] = useState<string>();
+    const [seat, setSeat] = useState<string>();
+
+
+
+    const handleSubmit = (event : any) =>{
+        event.preventDefault();
+        alert(`Email ${email} , Loc ${loc} , seat ${seat} , date from ${dates[0]} , date to ${dates[1]}`)
+    };
+
+
+
+
     return (
         <div className='Main-comp'>
+            <form onSubmit={handleSubmit}>
             <h1 className='Comp-heading'>Desk booking</h1>
             {/* Input div */}
             <div className='Input-div'>
-                <input placeholder='Email' className='Comp-input' style={{ marginTop: 5 }}></input>
-                <input placeholder='Name' className='Comp-input' style={{ marginTop: 5 }}></input>
+                <input placeholder='Email' className='Comp-input' style={{ marginTop: 5 }} type="text" value={email} onChange={(e) => {setEmail(e.target.value); console.log(email);}}></input>
+                <input placeholder='Name' className='Comp-input' style={{ marginTop: 5 }} value={name} onChange={(e) => {setName(e.target.value); console.log(name);}}></input>
             </div>
             {/* First dropdown menu div */}
             <div className='Drop-div'>
-                <Dropdown className='Drop-down' arrowClassName='Arrow-class' options={location} placeholder="Location" />
-                <Dropdown className='Drop-down' arrowClassName='Arrow-class' options={seatNum} placeholder="Desk number" />
+                <Dropdown className='Drop-down' arrowClassName='Arrow-class' options={locations} placeholder="Location" value={loc} onChange={(e) => {setLoc(e.value); console.log(loc);}}/>
+                <Dropdown className='Drop-down' arrowClassName='Arrow-class' options={seatNum} placeholder="Desk number" value={seat} onChange={(e) => {setSeat(e.value); console.log(seat);}}/>
             </div>
             {/* Second dropdown menu div */}
             <div className='Drop-div-2'>
@@ -38,7 +55,7 @@ export const BookingDesk: FC<bookingProps> = () => {
                 <Dropdown className='Drop-down' arrowClassName='Arrow-class' options={time} placeholder="Till" />
             </div>
             <div className='Calendar-div'>
-                <DateRangePicker className='calendar' showOneCalendar />
+                <DateRangePicker className='calendar' showOneCalendar value={dates} onChange={(event) => {setDates(event!); console.log(dates);} } />
 
             </div>
             {/* Logo div */}
@@ -48,8 +65,9 @@ export const BookingDesk: FC<bookingProps> = () => {
             {/* Description div + submit button */}
             <div className='Desc-div'>
                 <input placeholder='Description' className='Desc-input' ></input>
-                <button className='Submit-btn' >Submit</button>
+                <button className='Submit-btn' type="submit" >Submit</button>
             </div>
+            </form>
         </div>
     );
 };
