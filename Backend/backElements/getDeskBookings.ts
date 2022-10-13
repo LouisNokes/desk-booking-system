0b6c3e06b46be73bf1eb3d1c257ad5a79b6ad7e8
Louis Nokes <79872690+LouisNokes@users.noreply.github.com>
2022-10-13 13:42:24 +0100

    Additional changes to get bookings
    Forgot to add Gloucester

--- Backend/backElements/getDeskBookings.ts
+++ Backend/backElements/getDeskBookings.ts
@@ -18,19 +18,40 @@ const bookedDesk =
       "Dateto": '05/10/2022',
       "isBooked": false
 
-   },],
+   }],
+   "Gloucester": [{
+      "seatNum": 1,
+      "Date-fr": '03/10/2022',
+      "Date-to": '03/10/2022',
+      "isBooked": true
+   },
+   {
+      "seatNum": 1,
+      "Date-fr": '04/10/2022',
+      "Date-to": '04/10/2022',
+      "isBooked": true
+   },
+   {
+      "seatNum": 6,
+      "Datefr": '05/10/2022',
+      "Dateto": '05/10/2022',
+      "isBooked": false
+
+   }]
 };
 
 function getBookings(site: string, seatNum: string, datefr?: string, dateto?: string) {
-   const bookArr = bookedDesk.Manchester;
+   const bookArrMan = bookedDesk.Manchester;
+   const bookArrGlo = bookedDesk.Gloucester;
+
 
    if (site === "Manchester") {
-      const siteBookings = bookArr.filter(booking => {
+      const siteBookings = bookArrMan.filter(booking => {
          return booking.seatNum === parseInt(seatNum)
       })
       return siteBookings;
    } else if (site === "Gloucester") {
-      const siteBookings = bookArr.filter(booking => {
+      const siteBookings = bookArrGlo.filter(booking => {
          return booking.seatNum === parseInt(seatNum)
       })
       return siteBookings;
