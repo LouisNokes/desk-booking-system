7e0883a0a66f519e694d6707f3676da782802819
Louis Nokes <79872690+LouisNokes@users.noreply.github.com>
2022-10-19 13:55:36 +0100

    Retrieve data from api
    Now able to retrieve data from the api

--- src/components/desk.tsx
+++ src/components/desk.tsx
@@ -1,25 +1,15 @@
 import React, { FC } from 'react';
 import '../App.css';
 
-interface BookedProps {
-    deskNum: number
-}
 
-export const Desk: FC<BookedProps> = (deskNum) => {
+const Desk: FC<any> = ({ desk }) => {
     return (
         <div>
-            <div className='Desk-comp'>
-                <p style={{ color: 'black', fontSize: 15 }}>Desk 1</p>
-                <p style={{ color: 'black', fontSize: 15 }}>Location:</p>
-                <p style={{ color: 'green', fontSize: 15 }}>Available</p>
-            </div>
-            <div className='Desk-comp'>
-                <p style={{ color: 'black', fontSize: 15 }}>Desk 2</p>
-                <p style={{ color: 'black', fontSize: 15 }}>Location:</p>
-                <p style={{ color: 'red', fontSize: 15 }}>Booked: 10am - 2pm</p>
-            </div>
+            <p>{desk.seatNum}</p>
         </div>
     );
 };
+
+export default Desk;
 
 
