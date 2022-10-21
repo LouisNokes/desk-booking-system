a15135b4f2e1ca50be17161518103243d83a7182
Mahir Ahmed <79879216+Mahir-19@users.noreply.github.com>
2022-10-21 13:26:19 +0100

    created mysql.ts
    created the connection between nodejs and mysql to the database

---
+++ Backend/backElements/mysql.ts
@@ -0,0 +1,40 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'desk_savers'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM users', (err : any, res : any)=>{
+    return console.log(res)
+});
+
+connection.end();
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const {createPool} = require('mysql')
+
+const pool = createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  connectionLimit: 10
+})
+
+pool.query(`select * from users`, (err:any, res:any)=>{
+    return console.log(res)
+})*/
